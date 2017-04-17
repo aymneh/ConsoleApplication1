239bc9fc9c90c5dd7f5b1f18600fff54504697e1
EvoTech <EvoTech@DESKTOP-3D1DSAD>
2017-04-17 16:52:28 +0300

    add a comment

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,6 +10,9 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            //this is my first program
+            Console.WriteLine("hello aws word");
+            Console.ReadKey();
         }
     }
 }
